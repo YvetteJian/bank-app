38bac211ee0920ec08f434fcbc3ac1d21697adb1
Hailey Sun <60712169+sxw022020@users.noreply.github.com>
2023-05-17 20:25:25 -0700

    Update SignUp.js

--- bankapp/src/components/SignUp/SignUp.js
+++ bankapp/src/components/SignUp/SignUp.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+import React,{Component} from 'react';
+import axios from 'axios';
 
-export default function SignUp() {
-    const [username, setUsername] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [balance, setBalance] = useState(0);
 
-    const signUp = () => {
-        console.log(username+password+balance);
-        localStorage.setItem("user", username);
-        window.location.reload();
+export default class SignUp extends Component{
+    constructor(props){
+        super(props);
+        this.state = { 
+            username : null,
+            password :null,
+            balance: 0,
+            errorMsg:null
+        }
     }
 
-    return(
-        <div>
-            <input type="text" onChange={(event) => setUsername(event.currentTarget.value)} placeholder='Username'/>
-            <input type="password" onChange={(event) => setPassword(event.currentTarget.value)} placeholder='Password'/>
-            <input type="number" onChange={(event) => setBalance(event.currentTarget.value)} placeholder='Balance'/>
-            <button onClick={signUp}>Sign up</button>
-        </div>
-    );
+    signUp=()=>{
+        console.log(this.state.username+this.state.password+this.state.balance)
+        axios.defaults.withCredentials = true;
+        axios.post('http://localhost:8080/register', { username:this.state.username, password:this.state.password,balance:this.state.balance })
+        .then(response => {
+        console.log(response.data);
+        localStorage.setItem("user",this.state.username);
+        window.location.reload();
+    })
+    .catch(error => {
+        console.log(error)
+        this.setState({errorMsg:error.response.data})
+    });
+    }
+
+    render(){
+        return(
+            <div>
+                <input  type="text" onChange={(event)=>{ this.state.username = event.currentTarget.value;}} placeholder='Username'/>
+                <input  type="password" onChange={(event)=>{ this.state.password = event.currentTarget.value;}} placeholder='Password'/>
+                <input  type="number" onChange={(event)=>{ this.state.balance= event.currentTarget.value;}} placeholder='Balance'/>
+                <button onClick={this.signUp}>Sign up</button>
+                <div className='errorMsg'>{this.state.errorMsg}</div>
+            </div>
+         );
+    }
 }
